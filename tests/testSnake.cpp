9ee7f4b539650223395dadf52eddf790f4752221
Rakhman Karymshakov <karymrak@fel.cvut.cz>
2024-11-05 00:36:08 +0100

    in Snake initialization and death check test was added eat checking; was created test(Snake movement check).

--- tests/testSnake.cpp
+++ tests/testSnake.cpp
@@ -3,7 +3,8 @@
 
 /**
  * Test case to verify the Snake object's initialization, including the initial score,
- * alive status, body size, and positions of the segments. It also checks the state
+ * alive status, body size, and positions of the segments. It also checks the eat() method
+ * to verify the score increments when the snake eats and the state
  * after calling the die() method to ensure the snake is no longer alive.
  */
 TEST_CASE("Snake initialization and death check") {
@@ -20,6 +21,30 @@ TEST_CASE("Snake initialization and death check") {
     REQUIRE(body[2].x == 3);
     REQUIRE(body[2].y == GameData::MapDimensions::HEIGHT / 2);
 
+    snake.eat();
+    REQUIRE(snake.getScore() == 1);
+
     snake.die();
+    REQUIRE(snake.isAlive() == false);
+}
+
+TEST_CASE("Snake movement check") {
+    Snake snake;
+    snake.setDirection(GameData::Direction::RIGHT);
+    snake.update();
+    auto body = snake.getBody();
+    REQUIRE(body[0].x == 6);
+    REQUIRE(body[0].y == GameData::MapDimensions::HEIGHT / 2);
+
+    REQUIRE(body[1].x == 5);
+    REQUIRE(body[1].y == GameData::MapDimensions::HEIGHT / 2);
+
+    REQUIRE(body[2].x == 4);
+    REQUIRE(body[2].y == GameData::MapDimensions::HEIGHT / 2);
+    REQUIRE(snake.isAlive() == true);
+
+    for (int i = 0; i < (GameData::MapDimensions::WIDTH - 6); i++)
+        snake.update();
+
     REQUIRE(snake.isAlive() == false);
 }
